ed58aba42eeb506693375a7a3b439bb4548dc699
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2025-11-16 13:45:31 -0600

    fixed broken link

--- eval/download_dataset.sh
+++ eval/download_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-wget http://www.cs.virginia.edu/~skadron/lava/Rodinia/Packages/rodinia_3.1.tar.bz2
+wget http://www.cs.virginia.edu/~skadron/lava/rodinia/Packages/rodinia_3.1.tar.bz2
 tar -xvjf rodinia_3.1.tar.bz2 -C . --strip-components=1 rodinia_3.1/data
 rm rodinia_3.1.tar.bz2
 
